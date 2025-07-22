8c832fca1a626b7233ac2652f35cc5dd92333f8a
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-07-22 14:00:17 +0700

    add lec04 moo empty

---
+++ reactive-programming-playground/src/main/java/vn/edu/ptit/duongvct/reactive_programming_playground/sec02/Lec04MonoEmptyError.java
@@ -0,0 +1,18 @@
+package vn.edu.ptit.duongvct.reactive_programming_playground.sec02;
+
+import reactor.core.publisher.Mono;
+import vn.edu.ptit.duongvct.reactive_programming_playground.common.Util;
+
+public class Lec04MonoEmptyError {
+    public static void main(String[] args) {
+        getUsername(3)
+                .subscribe(Util.subscriber());
+    }
+    private static Mono<String> getUsername(int userId) {
+        return switch (userId) {
+            case 1 -> Mono.just("sam");
+            case 2 -> Mono.empty(); //null
+            default -> Mono.error(new RuntimeException("Invalid input"));
+        };
+    }
+}
