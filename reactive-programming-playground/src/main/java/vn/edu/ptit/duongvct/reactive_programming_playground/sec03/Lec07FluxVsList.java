79a5361f2e28b5f78339b58f323b5ea7a92fe94b
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-07-22 22:13:20 +0700

    update

--- reactive-programming-playground/src/main/java/vn/edu/ptit/duongvct/reactive_programming_playground/sec03/Lec07FluxVsList.java
+++ reactive-programming-playground/src/main/java/vn/edu/ptit/duongvct/reactive_programming_playground/sec03/Lec07FluxVsList.java
@@ -1,7 +1,21 @@
 package vn.edu.ptit.duongvct.reactive_programming_playground.sec03;
 
+import vn.edu.ptit.duongvct.reactive_programming_playground.common.Util;
+import vn.edu.ptit.duongvct.reactive_programming_playground.sec01.subscriber.SubscriberImpl;
+import vn.edu.ptit.duongvct.reactive_programming_playground.sec03.helper.NameGenerator;
+
 public class Lec07FluxVsList {
     public static void main(String[] args) {
-        
+        var list = NameGenerator.getNamesList(10);
+        System.out.println(list);
+
+        NameGenerator.getNamesFlux(10)
+                .subscribe(Util.subscriber());
+//        var subscriber = new SubscriberImpl();
+//        NameGenerator.getNamesFlux(10)
+//        .subscribe(Util.subscriber());
+//        subscriber.getSubscription().request(3);
+//        subscriber.getSubscription().cancel();
+
     }
 }
