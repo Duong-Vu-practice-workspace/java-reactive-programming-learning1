668245eaa2686c42749dcfecd33defedd06a6e6a
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-07-24 21:53:12 +0700

    feat:DefaultIfEmpty and SwichIfEmpty
    Add Lec07 and Lec08 file

--- reactive-programming-playground/src/main/java/vn/edu/ptit/duongvct/reactive_programming_playground/sec05/Lec08SwitchIfEmpty.java
+++ reactive-programming-playground/src/main/java/vn/edu/ptit/duongvct/reactive_programming_playground/sec05/Lec08SwitchIfEmpty.java
@@ -1,19 +1,19 @@
 package vn.edu.ptit.duongvct.reactive_programming_playground.sec05;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import vn.edu.ptit.duongvct.reactive_programming_playground.common.Util;
 
-import java.util.Optional;
-
-public class Lec07DefaultIfEmpty {
+public class Lec08SwitchIfEmpty {
+    public static final Logger log = LoggerFactory.getLogger(Lec08SwitchIfEmpty.class);
     public static void main(String[] args) {
-//        Mono.empty()
-//                .defaultIfEmpty("fallback")
-//                .subscribe(Util.subscriber());
         Flux.range(1, 10)
                 .filter(i -> i > 11)
-                .defaultIfEmpty(50)
+                .switchIfEmpty(fallback())
                 .subscribe(Util.subscriber());
+    }
+    private static Flux<Integer> fallback(){
+        return Flux.range(100, 3);
     }
 }
